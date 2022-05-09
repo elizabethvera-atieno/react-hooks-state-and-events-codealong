40fc400b6dfa3b424d8733ac26b5d5ab10c24326
elizabethvera-atieno <atienolizvera@gmail.com>
2022-05-09 23:52:32 +0300

    complete cod along

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,19 @@
 import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+// Set up the initia state
+const [isOn, setIsOn] = useState(false);
+
+// Call the setter function to update state
+function handleClick{
+  setIsOn((isOn) => !isOn)
+}
+
+const color = isOn ? "red" : "white";
+
+// Use the state variable in the component
+  return <button style={{ background: color }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
